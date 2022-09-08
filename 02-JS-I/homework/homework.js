ca8b8513411dbbe98b476f252506ab90e0150b54
danelmartinezz <martinezgdanel@hotmail.com>
2022-09-08 01:54:38 -0300

    First commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -2,6 +2,7 @@
 
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = null;
+const testeando = "Hola"; // es para usar git TODO:eliminar
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
